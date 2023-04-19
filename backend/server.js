3dae39fa104420c146644376a61da19ee45c1cdd
Arvind Nagalbone <arvindn@inboxops.local>
2023-04-20 03:30:42 +0530

    changes in ui

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-// hghjg
+const express = require('express');
+const connectDb = require('./config/config');
+//connection to database
+connectDb();
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send("<h1>WELCOM</h1>");
+});
+
+
+const PORT = 8080;
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`);
+})
---
+++ frontend/src/components/HomePage.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import {Table,Container,Row,Col} from 'react-bootstrap';
+import AddModel from './AddModel';
+
+const HomePage = () => {
+  return (
+    <>
+    <Container>
+        <Row>
+            <Col>
+            <br/>
+            <AddModel />
+            <hr/>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                    <th>Sr.No</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Mobile</th>
+                    <th>Address</th>
+                    <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <td>1</td>
+                    <td>yogesh Sabankar</td>
+                    <td>sabankar@gmail.com</td>
+                    <td>9563256451</td>
+                    <td>Pune</td>
+                    <td><i class="fas fa-edit"></i>&nbsp;&nbsp;<i class="fa fa-trash"></i></td>
+                    </tr>
+                    
+                    
+                </tbody>
+                </Table>
+            </Col>
+        </Row>
+    </Container>
+    </>
+  )
+}
+
+export default HomePage;
